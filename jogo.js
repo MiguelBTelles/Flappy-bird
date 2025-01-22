965eafab801c3f6a149cc3c0cf1e0a1178afe0de
Seu Nome <seuemail@example.com>
2025-01-22 13:12:15 -0300

    commit

--- jogo.js
+++ jogo.js
@@ -1,5 +1,10 @@
 console.log('[DevSoutinho] Flappy Bird');
 
+
+let frames = 0;
+const som_HIT = new Audio();
+som_HIT.src = './efeitos/hit.wav';
+
 const sprites = new Image();
 sprites.src = './sprites.png';
 
@@ -54,79 +59,143 @@ const planoDeFundo = {
         )
     }
 }
+function criaChao() {
+    const chao = {
+        spriteX: 0,
+        spriteY: 610,
+        largura: 224,
+        altura: 112,
+        x: 0,
+        y: canvas.height - 112,
 
-const chao = {
-    spriteX: 0,
-    spriteY: 610,
-    largura: 224,
-    altura: 112,
-    x: 0,
-    y: canvas.height - 112,
-    
-    desenha()  {
-        contexto.drawImage(
-            sprites,
-            chao.spriteX, chao.spriteY,
-            chao.largura, chao.altura,
-            chao.x, chao.y,
-            chao.largura, chao.altura,
-        );
+        atualiza() {
+            const movimentoDoChao = 1;
+            const repeteEm = chao.largura / 2;
+            const movimentacao = chao.x - movimentoDoChao;
+            chao.x = movimentacao % repeteEm;
+        },
 
-        contexto.drawImage(
-            sprites,
-            chao.spriteX, chao.spriteY,
-            chao.largura, chao.altura,
-            (chao.x + chao.largura), chao.y,
-            chao.largura, chao.altura,
-        )
-    }
-    
-};
+        desenha() {
+            contexto.drawImage(
+                sprites,
+                chao.spriteX, chao.spriteY,
+                chao.largura, chao.altura,
+                chao.x, chao.y,
+                chao.largura, chao.altura,
+            );
 
-const FlappyBird = {
-    spriteX: 0,
-    spriteY: 0,
-    largura: 33,
-    altura: 24,
-    x: 10,
-    y: 50,
-    gravidade: 0.25,
-    velocidade: 0,
+            contexto.drawImage(
+                sprites,
+                chao.spriteX, chao.spriteY,
+                chao.largura, chao.altura,
+                (chao.x + chao.largura), chao.y,
+                chao.largura, chao.altura,
+            )
+        }
 
-    atualiza() {
-        FlappyBird.velocidade = FlappyBird.velocidade + FlappyBird.gravidade;
-        FlappyBird.y = FlappyBird.y + FlappyBird.velocidade;
-    },
-
-    desenha() {
-
-        contexto.drawImage(
-            sprites,
-            FlappyBird.spriteX, FlappyBird.spriteY,
-            FlappyBird.largura, FlappyBird.altura,
-            FlappyBird.x, FlappyBird.y,
-            FlappyBird.largura, FlappyBird.altura,
-        );
-    }
+    };
+    return chao;
 }
+
+
+function fazColisao(flappyBird, chao) {
+    const flappyBirdY = flappyBird.y + flappyBird.altura;
+    const chaoY = chao.y;
+
+    if (flappyBirdY >= chaoY) {
+        return true;
+    }
+
+    return false;
+}
+
+function criaFlappyBird() {
+    const FlappyBird = {
+        spriteX: 0,
+        spriteY: 0,
+        largura: 33,
+        altura: 24,
+        x: 10,
+        y: 50,
+        gravidade: 0.25,
+        velocidade: 0,
+        pulo: 4.6,
+        pula() {
+            FlappyBird.velocidade = -FlappyBird.pulo
+        },
+
+        atualiza() {
+            if (fazColisao(FlappyBird, globais.chao)) {
+                som_HIT.play();
+                mudaParaTela(Telas.INICIO);
+                return;
+            }
+
+            FlappyBird.velocidade = FlappyBird.velocidade + FlappyBird.gravidade;
+            FlappyBird.y = FlappyBird.y + FlappyBird.velocidade;
+        },
+
+        movimentos: [
+            { spriteX: 0, spriteY: 0 },
+            { spriteX: 0, spriteY: 26 },
+            { spriteX: 0, spriteY: 52 },
+        ],
+        frameAtual: 0,
+        atualizaOFrameAtual() {
+            const intervaloDeFrames = 10;
+
+            const passouOIntervalo = frames % intervaloDeFrames === 0;
+            if (passouOIntervalo) {
+                const baseDoIncremento = 1;
+                const incremento = baseDoIncremento + FlappyBird.frameAtual;
+                const baseRepeticao = FlappyBird.movimentos.length;
+                FlappyBird.frameAtual = incremento % baseRepeticao;
+            }
+        },
+        desenha() {
+            FlappyBird.atualizaOFrameAtual();
+            const { spriteX, spriteY } = FlappyBird.movimentos[FlappyBird.frameAtual];
+
+            contexto.drawImage(
+                sprites,
+                spriteX, spriteY,
+                FlappyBird.largura, FlappyBird.altura,
+                FlappyBird.x, FlappyBird.y,
+                FlappyBird.largura, FlappyBird.altura,
+            );
+        }
+    }
+    return FlappyBird;
+}
+
+const globais = {};
 let telaAtiva = {};
 function mudaParaTela(novaTela) {
     telaAtiva = novaTela;
+
+    if (telaAtiva.inicializa) {
+        telaAtiva.inicializa();
+    }
 };
 
 const Telas = {
     INICIO: {
+        inicializa() {
+            globais.flappyBird = criaFlappyBird();
+            globais.chao = criaChao();
+        },
+
         desenha() {
             planoDeFundo.desenha();
-            chao.desenha();
-            FlappyBird.desenha();
+            globais.chao.desenha();
+            globais.flappyBird.desenha();
             mensagemGetReady.desenha();
         },
         click() {
             mudaParaTela(Telas.JOGO);
         },
         atualiza() {
-
+            globais.chao.atualiza();
         }
     }
 };
@@ -134,24 +203,29 @@ const Telas = {
 Telas.JOGO = {
     desenha() {
         planoDeFundo.desenha();
-        chao.desenha();
-        FlappyBird.desenha();
+        globais.chao.desenha();
+        globais.flappyBird.desenha();
+    },
+    click() {
+        globais.flappyBird.pula();
     },
     atualiza() {
-        FlappyBird.atualiza();
-}
+        globais.flappyBird.atualiza();
+    }
 };
 
 function loop() {
-    
+
     telaAtiva.desenha();
     telaAtiva.atualiza();
-    
+
+    frames = frames + 1;
+
     requestAnimationFrame(loop);
 }
 
-window.addEventListener('click', function() {
-    if(telaAtiva.click) {
+window.addEventListener('click', function () {
+    if (telaAtiva.click) {
         telaAtiva.click();
     }
 })
